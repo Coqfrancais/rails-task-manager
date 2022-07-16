0064e6d21b9bb5d10d783636f29a39d6458c9238
ulysse galet <ulysse.galet@rennes-sb.com>
2022-07-16 14:50:38 +0200

    tasks controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  #Ne pas utiliser ressources, on va tout faire à la main
+
+  #1 - Lister toutes les tâches 
+  get "tasks" => "tasks#index"
+
 end
