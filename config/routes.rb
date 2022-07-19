2e6e560b1f41fbdfc03e15c28df171a9abade8e0
ulysse galet <ulysse.galet@rennes-sb.com>
2022-07-19 18:39:21 +0200

    exo tasks

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,12 @@ Rails.application.routes.draw do
   #1 - Lister toutes les tâches 
   get "tasks" => "tasks#index"
 
+  #2 - Lister une tâche en particulier
+  get "tasks/:id" => "tasks#show", as: :task #<= création d'un allias
+
+  #création d'une tâche (en deux étapes)
+  get "tasks/new" => "tasks#new"
+  post "tasks" => "tasks#create"
+
+
 end
